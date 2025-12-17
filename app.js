8fe8a1a0eca1fa0c5f3af467635c0ee5018f475e
Nurzhan <nurzhqn@gmail.com>
2025-12-17 16:26:31 +0500

    fix: delete cors

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const cors = require("cors");
 const path = require("path");
 const app = express();
 const PORT = 3000;
@@ -8,17 +7,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // CORS
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-
-  next();
-});
 
 app.use("/public", express.static(path.join(__dirname, "public")));
 
